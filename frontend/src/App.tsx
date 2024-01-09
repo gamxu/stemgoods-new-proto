77badfec8a36f4eebabb1f96bfa761e34bd45afe
gamxu <wuttipat.did@gmail.com>
2024-01-09 14:30:23 +0700

    fecth picture api and display in grid

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,14 @@
-import './App.css'
-import MansonryGrid from './components/MansonryGrid'
-import Navbar from './components/navbar'
+import "./App.css";
+import MansonryGrid from "./components/MansonryGrid";
+import Navbar from "./components/navbar";
 
 function App() {
-
   return (
     <>
-    <Navbar/>
-    <MansonryGrid/>
+      <Navbar />
+      <MansonryGrid />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
